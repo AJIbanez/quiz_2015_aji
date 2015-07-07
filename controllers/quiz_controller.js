29a7f2f6b8ecd59db60a9ad896b0ef580550885d
AJIbanez <dspacep@gmail.com>
2015-07-07 14:31:05 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -101,6 +101,34 @@ exports.create=function(req,res) {
        ); 
 };
 
+// PUT /quizes/id
+exports.update=function(req,res) {
+   req.quiz.pregunta = req.body.quiz.pregunta;
+   req.quiz.respuesta = req.body.quiz.respuesta;
+
+   req.quiz
+   .validate()
+   .then(
+	function(err){
+	  if(err){
+	     res.render('quizes/edit',{quiz: quiz, errors:  err.errors});
+          } else {
+		req.quiz //save: guarda campos pregunta y respuesta en DB
+   		.save({fields:["pregunta","respuesta"]})
+		.then(function() {
+      		res.redirect('/quizes');});
+           }  // res.redirect: redirección HTTP a lista de de preguntas (URL relativo)
+        }
+       ); 
+};
+
+
+//Get /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz=req.quiz; // autoload de instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+	
 //Get /author
 exports.author = function(req,res) {
 	res.render('author');
